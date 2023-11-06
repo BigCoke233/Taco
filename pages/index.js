e452942f67e68648c28609c229b50d5a8337ac95
Eltrac <hi@guhub.cn>
2023-11-06 23:37:06 +0800

    feature(blog): 基本实现博客文章获取，部分样式书写

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,101 @@
+/**
+ * index.js - 博客首页
+ * 
+ *  - 页面头部
+ *  - 导航
+ *  - 博客文章展示
+ *  - 社交媒体
+ *  - 自述
+ * 
+ * @authro Eltrac
+ * @date 2023/11/6
+ */
+
+/* Imports - 引用 */
+
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+/**
+ * 获取博客文章列表
+ * 
+ * @returns object
+ */
 
-export default function Home() {
+export async function getStaticProps() {
+  const res = await fetch('http://43.143.130.74/api/posts')
+  const posts = await res.json()
+
+  return { props: { posts } }
+}
+
+/**
+ * 最新博文展示
+ *
+ * @returns 
+ */
+
+function BlogLatest({ posts, className }) {
+  //获取第一篇博文
+  var post = posts.data.dataSet[0]
+
+  //在文章内容中寻找 <!--more--> 标签
+  // 若存在，则截取 <!--more--> 之前的内容
+  // 若不存在，则截取前 150 个字符
+  var moreTag = post.digest.search(/<!--more-->/);
+  var sliceEnd = (moreTag!=-1) ? moreTag- 3 : 150
+  
+  //删除 html 标签，只保留文字内容，然后执行截取操作
+  var digest = post.digest.replace(/<\/?[^>]+(>|$)/g, "").slice(0,sliceEnd);
+
+  //样式
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <article id="blog-latest" className={className}>
+      <h2 className="text-5xl font-extrabold">
+        <a href={"/blog/"+post.slug}>{post.title}</a>
+      </h2>
+      <p className="text-lg my-5">{digest}</p>
+    </article>
+  )
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+/**
+ * 往期博文列表
+ *
+ * @returns 
+ */
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function BlogList({ posts, className }) {
+  
+  var list = posts.data.dataSet
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  return (
+    <div id="blog-list" className={className}>
+        {list.map((post) => {
+          return (
+            <article>
+              <h2><a href={"/blog/"+post.slug}>{post.title}</a></h2>
+            </article>
+          )
+        })}
+    </div>
+  )
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+/**
+ * 页面主要内容
+ * 
+ * @returns page
+ */
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <section id="blog" className="md:flex md:gap-10">
+        <BlogLatest posts={posts} className="md:w-2/3" />
+        <BlogList posts={posts} className="md:w-1/3" />
+      </section>
+      
+    </>
   )
 }
---
+++ pages/layout.js
@@ -0,0 +1,19 @@
+/**
+ * layout.js - 全局布局
+ * 
+ *  - 页面容器样式
+ *  - 引入 Google Font
+ * 
+ * @authro Eltrac
+ * @date 2023/11/6
+ */
+
+export default function Layout({ children }) {
+    return (
+        <div id="app" 
+        className="max-w-4xl mx-auto my-20 p-5 md:p-0">
+            <style jsx>{`@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300;400;500;600;700;900&display=swap');`}</style>
+            {children}
+        </div>
+    )
+}
