1770b8b560cdb8c926c548d26a9639edd9dd6345
Eltrac <hi@guhub.cn>
2023-11-08 23:46:22 +0800

    fix(css): try to initialize tailwind class

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import Layout from './layout.js'
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <div id="tailwindClassInitializer" className={process.env.pagePadding} />
       <Component {...pageProps} />
     </Layout>
   )
