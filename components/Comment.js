ed79336e020bc52169218fcfdb0ef7d60522a565
Eltrac <hi@guhub.cn>
2023-11-26 23:38:19 +0800

    doc: 暂时移除评论区文字提示

--- components/Comment.js
+++ components/Comment.js
@@ -9,7 +9,7 @@ import Link from 'next/link'
 export default function Comment() {
     return (
         <section id="comment" className="text-center mb-10">
-            <p>评论功能尚未完成，敬请期待</p>
+            <p></p>
         </section>
     )
   }
