f18435a7188d7f3abfdfa6b6900123b0685de524
Eltrac <hi@guhub.cn>
2023-11-28 13:54:04 +0800

    perf(helpbar): 修改右侧菜单的样式

--- components/Helpbar.js
+++ components/Helpbar.js
@@ -24,7 +24,11 @@ export default function Helpbar() {
             if ("link" == item.type)
             {
                 return (
-                    <Link href={item.link} key={item.key}>{item.content}</Link>
+                    <Link 
+                        href={item.link} key={item.key}
+                        className="border rounded-full p-2 bg-gray-50 shadow transition
+                        hover:border-lime-700 hover:text-white hover:bg-lime-700"
+                    >{item.content}</Link>
                 )
             }
         })}
