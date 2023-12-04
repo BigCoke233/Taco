e6af6270462b7fc5b66c5720c068fe7dc06c5394
Eltrac <hi@guhub.cn>
2023-12-04 14:27:35 +0800

    fix: 解决缺少 key 的问题

--- components/index/Nav.js
+++ components/index/Nav.js
@@ -12,7 +12,7 @@ export default function Nav({ links }) {
         md:mx-10 mt-16 mb-8 gap-8 items-center">
             {links.list.map((item) => {
                 return (
-                    <Link href={item.link} target="_blank">
+                    <Link href={item.link} target="_blank" key={item.title}>
                         <div className="p-5 relative transition border
                         shadow bg-gray-50 hover:shadow-lg hover:-translate-y-2">
                             <div className="absolute inset-x-0 -top-3 mx-auto
