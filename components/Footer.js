4140aa29d70e3dfe2e4808d2137da03c6aa15f94
Eltrac <hi@guhub.cn>
2023-12-07 13:07:06 +0800

    fix: try fixing image component 400 request

--- components/Footer.js
+++ components/Footer.js
@@ -7,6 +7,8 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
+import beian from '@/public/beian.png'
+
 export default function Footer() {
     return (
       <footer id="footer" className="text-center text-gray-500 text-sm
@@ -17,7 +19,7 @@ export default function Footer() {
         <section id="icp">
           <p><Link href="https://beian.miit.gov.cn/" target="_blank">渝 ICP 备 2023014837 号</Link></p>
           <p className="align-middle">
-            <Image src="/beian.png" alt="备案图标" width={14} height={14} className="inline mr-1" /> 
+            <Image src={beian} alt="备案图标" width={14} height={14} className="inline mr-1" /> 
             <a href="https://beian.mps.gov.cn/#/query/webSearch?code=50011702500934" rel="noreferrer" target="_blank">渝公网安备 50011702500934</a>
           </p>
         </section>
