c49830702ed9efe9b025cb2fe163160443574f56
Eltrac <hi@guhub.cn>
2023-12-11 23:43:20 +0800

    perf: 微调样式

--- components/Footer.js
+++ components/Footer.js
@@ -8,11 +8,13 @@ import Link from 'next/link'
 
 export default function Footer() {
     return (
-      <footer id="footer" className="text-center text-gray-500 text-sm
+      <footer id="footer" className="md:text-center text-gray-500 text-sm
       mt-10 p-5 md:p-0 flex justify-between items-center">
+
         <section id="copyright">
           <p>&copy; 2019-2023 Eltrac</p>
         </section>
+
         <section id="icp" className="text-right md:text-center">
           <p><Link href="https://beian.miit.gov.cn/" target="_blank">渝 ICP 备 2023014837 号</Link></p>
           <p className="align-middle">
@@ -20,6 +22,7 @@ export default function Footer() {
             <a href="https://beian.mps.gov.cn/#/query/webSearch?code=50011702500934" rel="noreferrer" target="_blank">渝公网安备 50011702500934</a>
           </p>
         </section>
+
         <section id="info" className="hidden md:block">
           <p>Powered by <Link href="/blog/bizzare-practice-of-indie-blog">Taco</Link></p>
         </section>
