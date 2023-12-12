c0de186f5cdee22920a5f5c740eaa84400f2eecf
Eltrac <hi@guhub.cn>
2023-12-12 20:05:47 +0800

    feat(helpbar): 增加返回顶部按钮，优化帮助栏链接

--- components/Helpbar.js
+++ components/Helpbar.js
@@ -6,8 +6,9 @@
  */
 
 import Link from 'next/link'
+import { Tooltip } from "@nextui-org/tooltip";
 
-import { LuHome } from "react-icons/lu";
+import { LuHome, LuPointer } from "react-icons/lu";
 
 export default function Helpbar() {
     const links = [
@@ -15,20 +16,44 @@ export default function Helpbar() {
             "type": "link",
             "link": "/",
             "content": (<LuHome />),
-            "key": "home"
+            "key": "home",
+            "tooltip": "首页"
+        },
+        {
+            "type": "action",
+            "action": () => window.scrollTo({top: 0, behavior: 'smooth'}),
+            "content": (<LuPointer />),
+            "key": "goTop",
+            "tooltip": "返回顶部"
         }
     ]
+
+    const buttonStyle = "border rounded-full bg-gray-50 shadow transition " + 
+    "p-2 md:p-3 text-lg " +
+    "hover:border-lime-700 hover:text-white hover:bg-lime-700";
+
     return(
-        <div className="fixed top-5 right-5 flex flex-wrap">
+        <div className="fixed top-5 right-5 
+        md:top-auto md:bottom-7 md:right-7
+        flex flex-wrap flex-col gap-3">
         {links.map((item) => {
             if ("link" == item.type)
             {
                 return (
-                    <Link 
-                        href={item.link} key={item.key}
-                        className="border rounded-full p-2 bg-gray-50 shadow transition
-                        hover:border-lime-700 hover:text-white hover:bg-lime-700"
-                    >{item.content}</Link>
+                    <Tooltip content={item.tooltip} placement="left" offset={15}>
+                        <Link 
+                            href={item.link} key={item.key}
+                            className={buttonStyle}
+                        >{item.content}</Link>
+                    </Tooltip>
+                )
+            }
+            else if ("action" == item.type) {
+                return (
+                    <Tooltip content={item.tooltip} placement="left" offset={15}>
+                        <button className={buttonStyle} key={item.key}
+                        onClick={item.action}>{item.content}</button>
+                    </Tooltip>
                 )
             }
         })}
