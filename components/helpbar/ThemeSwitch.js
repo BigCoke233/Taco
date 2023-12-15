51ce3bf1b62014164037539cf37c392011bd494d
Eltrac <hi@guhub.cn>
2023-12-15 18:49:02 +0800

    feat(theme): 检测系统设置并切换主题

--- components/helpbar/ThemeSwitch.js
+++ components/helpbar/ThemeSwitch.js
@@ -15,15 +15,24 @@ import { LuSunMoon } from "react-icons/lu"
 import Toast from '@/lib/useToast'
 
 import { setStore, getStore } from '@/lib/useLocalStorage'
+import { useEffect, useState } from "react";
 
 /* === 工具函数 === */
+
+/**
+ * SwitchTheme()
+ * 基本的切换主题操作
+ * 
+ * @param {boolean} [auto=false] 
+ * 该参数表示切换操作是否为自动执行而非用户操作
+ */
 
 function SwitchTheme() {
     //完成切换操作
     const Html = document.getElementsByTagName('html')[0]
     Html.classList.toggle('dark')
 
-    //存入 local Storage
+    //存入 Local Storage
     //发送 toast 提示操作完成
     if (Html.classList.contains('dark')) {
         setStore('siteTheme', 'dark')
@@ -35,9 +44,33 @@ function SwitchTheme() {
     }
 }
 
+/**
+ * SwitchByMedia()
+ * 
+ * 检测系统设置
+ * 并自动切换主题
+ */
+
+function SwitchByMedia() {
+    const [rendered, setRendered] = useState(false);
+
+    useEffect(() => {
+        const Html = document.getElementsByTagName('html')[0]
+        const theme = getStore('siteTheme')
+    
+        if (rendered) { //跳过第一次渲染
+            let systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches //系统是否为深色主题
+            let prefersLight = (theme == 'light') //用户是否偏好浅色主题
+    
+            if(systemDark && !prefersLight) Html.classList.add('dark')
+        } else setRendered(true)
+    })
+}
+
 /* === 主函数 === */
 
 export default function ThemeSwitch({ className }) {
+    SwitchByMedia()
     return (
         <li key="themeSwitch">
             <Tooltip content="切换主题" placement="left" offset={15}>
