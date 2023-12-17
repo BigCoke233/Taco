b1087d5f0ce1f03ce1375aa96081ab4d7a55a52c
Eltrac <hi@guhub.cn>
2023-12-17 14:57:33 +0800

    perf: 优化页面标题在移动端的显示

---
+++ components/utils/Heading.js
@@ -0,0 +1,14 @@
+/**
+ * Heading.js - 页面标题
+ * 
+ * @return jsx
+ */
+
+export default function Heading({ children, className, sub="" }) {
+    return (
+        <header className={`md:flex md:items-end md:gap-6 ${className}`}>
+            <h1 className="text-2xl md:text-3xl md:text-5xl font-extrabold">{children}</h1>
+            <p className="md:text-xl text-gray-600 font-semibold">{sub}</p>
+        </header>
+    )
+}
