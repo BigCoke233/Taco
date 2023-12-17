b38b8f60c3976235e2d88b27007041187a6aad08
Eltrac <hi@guhub.cn>
2023-12-17 14:59:31 +0800

    fix: 标题改崩了妈的

--- components/utils/Heading.js
+++ components/utils/Heading.js
@@ -7,7 +7,7 @@
 export default function Heading({ children, className, sub="" }) {
     return (
         <header className={`md:flex md:items-end md:gap-6 ${className}`}>
-            <h1 className="text-2xl md:text-3xl md:text-5xl font-extrabold">{children}</h1>
+            <h1 className="text-2xl md:text-5xl font-extrabold">{children}</h1>
             <p className="md:text-xl text-gray-600 font-semibold">{sub}</p>
         </header>
     )
