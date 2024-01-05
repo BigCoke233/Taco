b18803a5d66ed494a5a3658ed6a1c119edabc85b
Eltrac <hi@guhub.cn>
2024-01-05 20:50:45 +0800

    feat(ui): add submit button

--- components/comment/CommentForm.js
+++ components/comment/CommentForm.js
@@ -7,6 +7,7 @@
 
 import { PostComment } from "@/lib/postComment";
 import { Input, Textarea } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
 
 /* === 主函数 表单部分 === */
 
@@ -15,10 +16,11 @@ export default function CommentForm({ slug, token }) {
         <section id="comment-form"
             className="m-5">
             <Textarea variant="underlined" placeholder="留下你的智慧" />
-            <div className="grid grid-cols-3 gap-5">
+            <div className="grid grid-cols-4 gap-5">
                 <Input variant="underlined" type="name" label="*称呼" />
                 <Input variant="underlined" type="email" label="*邮箱" />
                 <Input variant="underlined" type="url" label="链接" />
+                <Button>发送评论</Button>
             </div>
         </section>
     )
