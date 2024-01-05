bc18a7c4f30baea293d8a3ccbfddec42aa78d610
Eltrac <hi@guhub.cn>
2024-01-05 18:08:53 +0800

    feat(ui): comment form interface

--- components/comment/CommentForm.js
+++ components/comment/CommentForm.js
@@ -6,16 +6,20 @@
  */
 
 import { PostComment } from "@/lib/postComment";
-import { Input } from "@nextui-org/input";
+import { Input, Textarea } from "@nextui-org/input";
 
 /* === 主函数 表单部分 === */
 
 export default function CommentForm({ slug, token }) {
     return (
-        <section id="comment-form">
-            <Input type="name" label="*称呼" />
-            <Input type="email" lable="*邮箱" />
-            <Input type="url" label="链接" />
+        <section id="comment-form"
+            className="m-5">
+            <Textarea variant="underlined" placeholder="留下你的智慧" />
+            <div className="grid grid-cols-3 gap-5">
+                <Input variant="underlined" type="name" label="*称呼" />
+                <Input variant="underlined" type="email" label="*邮箱" />
+                <Input variant="underlined" type="url" label="链接" />
+            </div>
         </section>
     )
 }
