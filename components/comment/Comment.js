3353c2c344a453755107bf1fbab2762ebf593ce0
Eltrac <hi@guhub.cn>
2024-01-07 22:43:39 +0800

    revert: 停止使用 Waline

--- components/comment/Comment.js
+++ components/comment/Comment.js
@@ -4,13 +4,11 @@
  * @returns jsx
  */
 import GiscusComment from "../comment/Giscus"
-import { Waline } from "./Waline"
 
 export default function Comment() {
     return (
         <section id="comment" className={`px-5 md:px-16`}>
             <GiscusComment />
-            <Waline serverURL="https://waline.guhub.cn" dark=".dark" />
         </section>
     )
 }
