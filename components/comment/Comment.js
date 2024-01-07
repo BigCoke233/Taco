994b1982c31a1915c27f2c2c8513764a2d538256
Eltrac <hi@guhub.cn>
2024-01-07 21:36:57 +0800

    chore(troubleshooting): 测试是否是 Tab 导致问题

--- components/comment/Comment.js
+++ components/comment/Comment.js
@@ -8,28 +8,20 @@
 
 import GiscusComment from "./Giscus"
 
-import '@montagejs/react-waline-client/dist/style/index.css'
 import ReactWalineClient from "@montagejs/react-waline-client"
+import '@montagejs/react-waline-client/dist/style/index.css'
 
-import {Tabs, Tab} from "@nextui-org/tabs"
+import {Accordion, AccordionItem} from "@nextui-org/accordion";
 
 export default function Comment() {
     return (
         <section id="comment" className={`px-5 md:px-16`}>
-            <Tabs aria-label="Options"
-                classNames={{
-                    base: 'flex justify-center',
-                    tabList: 'bg-transparent rounded-sm',
-                    tab: 'bg-transparent',
-                    cursor: 'rounded-sm bg-zinc-50 dark:bg-zinc-800'
-                }}>
-                <Tab key="waline" title="Waline">
-                    <ReactWalineClient serverURL="https://waline.guhub.cn/" dark=".dark" />
-                </Tab>
-                <Tab key="giscus" title="Giscus">
+            <ReactWalineClient serverURL="https://waline.guhub.cn/" dark=".dark" />
+            <Accordion>
+                <AccordionItem key="1" aria-label="Giscus" title="使用 Giscus">
                     <GiscusComment />
-                </Tab>
-            </Tabs>
+                </AccordionItem>
+            </Accordion>
         </section>
     )
 }
