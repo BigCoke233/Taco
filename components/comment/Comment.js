79f6847c0389ab11161bc879205f5abac2a8ce3d
Eltrac <hi@guhub.cn>
2024-01-07 22:40:18 +0800

    test waline

--- components/comment/Comment.js
+++ components/comment/Comment.js
@@ -1,16 +1,16 @@
-'use client'
-
 /**
  * 评论
  * 
  * @returns jsx
  */
 import GiscusComment from "../comment/Giscus"
+import { Waline } from "./Waline"
 
 export default function Comment() {
     return (
         <section id="comment" className={`px-5 md:px-16`}>
             <GiscusComment />
+            <Waline serverURL="https://waline.guhub.cn" dark=".dark" />
         </section>
     )
 }
