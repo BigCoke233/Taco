d7e2fae84ed1dc7c8469349e306de89dbc7b0011
Eltrac <hi@guhub.cn>
2024-01-08 08:08:03 +0800

    fix: 暂时禁用 Waline

--- components/comment/Comment.js
+++ components/comment/Comment.js
@@ -15,11 +15,12 @@ export default function Comment() {
     return (
         <section id="comment" className={`px-5 md:px-16`}>
             <Tabs aria-label="Options">
-                <Tab key="waline" title="waline">
-                    <Waline serverURL="https://waline.guhub.cn" dark=".dark" />
-                </Tab>
                 <Tab key="giscus" title="Giscus">
                     <GiscusComment />
+                </Tab>
+                <Tab key="waline" title="waline">
+                    <p>暂时不可用</p>
+                    {/*<Waline serverURL="https://waline.guhub.cn" dark=".dark" />*/}
                 </Tab>
             </Tabs>
         </section>
