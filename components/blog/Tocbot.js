04689ccf0b02117cd6130805d2ae0af7f3167adb
Eltrac <hi@guhub.cn>
2024-01-08 12:00:23 +0800

    style(DOM): 优化 DOM 树结构

--- components/blog/Tocbot.js
+++ components/blog/Tocbot.js
@@ -68,31 +68,33 @@ export default function Tocbot() {
         BindTocLinks()
     })
     return (
-        <aside id="tocbot-container" 
+        <aside id="toc">
+            <div id="tocbot-container" 
+            
+            className={`folded top-[5.5rem]
+            max-xl:bg-gray-100/[.85] dark:max-xl:bg-zinc-900/[.85] 
+            max-xl:backdrop-blur-md max-xl:shadow
+            max-xl:fixed max-xl:bottom-0 max-xl:inset-x-0
+            max-xl:transition-all max-xl:duration-3000
 
-        className={`folded top-[5.5rem]
-        max-xl:bg-gray-100/[.85] dark:max-xl:bg-zinc-900/[.85] 
-        max-xl:backdrop-blur-md max-xl:shadow
-        max-xl:fixed max-xl:bottom-0 max-xl:inset-x-0
-        max-xl:transition-all max-xl:duration-3000
+            md:fixed md:bottom-20 md:inset-x-32 md:h-auto
+            md:border md:border-zinc-200 md:dark:border-zinc-800 md:shadow-lg
+            md:rounded
 
-        md:fixed md:bottom-20 md:inset-x-32 md:h-auto
-        md:border md:border-zinc-200 md:dark:border-zinc-800 md:shadow-lg
-        md:rounded
+            lg:inset-x-72
 
-        lg:inset-x-72
-
-        xl:absolute xl:bottom-0 xl:pt-[25rem]
-        xl:transition-opacity xl:border-0
-        xl:h-full xl:shadow-none xl:bg-transparent
-        xl:inset-x-auto
-        `}>
-            <style jsx>{tocbotStyle}</style>
-            <header className="border-b border-zinc-300 dark:border-zinc-700 max-xl:px-5 max-xl:py-3 xl:hidden">
-                <h2 className="text-center font-bold">文章目录</h2>
-            </header>
-            <article id="tocbot" className="xl:sticky xl:top-12 md:text-lg
-            max-xl:py-2" />
+            xl:absolute xl:bottom-0 xl:pt-[25rem]
+            xl:transition-opacity xl:border-0
+            xl:h-full xl:shadow-none xl:bg-transparent
+            xl:inset-x-auto
+            `}>
+                <style jsx>{tocbotStyle}</style>
+                <header className="border-b border-zinc-300 dark:border-zinc-700 max-xl:px-5 max-xl:py-3 xl:hidden">
+                    <h2 className="text-center font-bold">文章目录</h2>
+                </header>
+                <article id="tocbot" className="xl:sticky xl:top-12 md:text-lg
+                max-xl:py-2" />
+            </div>
         </aside>
     )
 }
