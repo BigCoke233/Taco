e05312b426d873c92ccc00b2ef6de3b6b658d9f0
Eltrac <hi@guhub.cn>
2024-01-11 15:14:55 +0800

    chore(data): 将文章分类数据迁移到本地

---
+++ data/categories.data.js
@@ -0,0 +1,34 @@
+/**
+ * 文章分类
+ * 
+ * @file /data/categories.data.js
+ * @exports object
+ */
+
+const categories = {
+    "pieces": {
+        slug: "pieces",
+        name: "随笔",
+        description: "无处安放的有感而发。"
+    },
+    "essays": {
+        slug: "essays",
+        name: "杂谈",
+        description: "整齐排列的想法，和时不时的长篇大论。"
+    },
+    "logs": {
+        slug: "logs",
+        name: "记录",
+        description: "有时候做了什么就会想要写下来。"
+    },
+    "fictions": {
+        slug: "fictions",
+        name: "小说",
+        description: "于另一个世界的文字旅行。"
+    },
+    "poetry": {
+        slug: "poetry",
+        name: "诗歌",
+        description: "前言不接后语的流动文字。"
+    }
+}
