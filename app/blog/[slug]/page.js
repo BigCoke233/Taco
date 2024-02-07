96951e848b8c32ceabfd7bc54f46b14bb7bc0a16
Eltrac <hi@guhub.cn>
2024-02-07 20:53:08 +0800

    refactor(blog): half-switch to markson as a test

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -21,7 +21,10 @@ import Tocbot from "@/app/blog/components/Tocbot";
 import {Chip} from "@nextui-org/chip";
 
 //数据
+import { markson } from 'marksonjs';
 import categories from '@/data/categories.data';
+
+import path from 'path'
 
 /* === 元信息 === */
 
@@ -42,9 +45,9 @@ export async function generateMetadata({ params }) {
 export default async function Page({ params }) {
     try {
         //动态引入页面数据
-        const Page = await import(`data/posts/${params.slug}.md`).then((module)=>{
-            const matter = module.attributes;
-            return (
+          const data = markson.read(path.join(process.cwd(), `./data/posts/${params.slug}.md`)); 
+          const matter = data.attributes;
+          return (
                 <>
                     <Header 
                         banner={matter.banner?.img} 
@@ -70,15 +73,11 @@ export default async function Page({ params }) {
                             </div>
                         </header>
                         <Tocbot />
-                        <Content md={true}>{module.body}</Content>
+                        <Content>{data.html}</Content>
                     </article>
                     <Comment />
                 </>
             )
-        })
-
-        return Page;
-
     } catch(error) {
         //如果找不到对应的页面，则报出 404 错误
         if (error.code === 'MODULE_NOT_FOUND') 
